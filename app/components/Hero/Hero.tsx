611fb11ed05c8d280ee58377fde5ce8bdca2819f
Cristian Sagula <cristian@Cristians-MacBook-Pro.local>
2025-03-18 12:31:14 -0300

    last chages on h1 in hero mobile

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -50,7 +50,11 @@ const Hero = ({ dict, lang }: HeroProps) => {
         .to('.text-button', { autoAlpha: 1, y: 0, duration: DEFAULT_DURATION, ease: 'power2.out' }, '-=0.5');
 
       mobileTimeline
-        .to('.main-title-mobile', { x: 0, rotate: 0, duration: DEFAULT_DURATION * 2, ease: 'power2.inOut' }, '-=0.5')
+        .to(
+          '.main-title-mobile',
+          { opacity: 1, y: 0, rotate: 0, duration: DEFAULT_DURATION, ease: 'power2.out' },
+          '-=0.5'
+        )
         .to('.lead-mobile', { autoAlpha: 1, x: 0, duration: DEFAULT_DURATION, ease: 'power2.out' })
         .to('.text-button-mobile', { autoAlpha: 1, y: 0, duration: DEFAULT_DURATION, ease: 'power2.out' }, '-=0.5');
     },
@@ -87,7 +91,7 @@ const Hero = ({ dict, lang }: HeroProps) => {
           {dict.title}
         </h1>
 
-        <h1 className='main-title-mobile text-[3.5rem] lg:text-[7rem] text-primary-white leading-[96%] mb-[3.125rem] lg:mb-[7.125rem] -translate-x-40 block lg:hidden'>
+        <h1 className='main-title-mobile text-[3.5rem] lg:text-[7rem] text-primary-white leading-[96%] mb-[3.125rem] lg:mb-[7.125rem] opacity-0 translate-y-10 block lg:hidden'>
           {dict.title}
         </h1>
 
