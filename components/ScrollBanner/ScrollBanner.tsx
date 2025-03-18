8f6170690ddf9411ef61e3672f7225b083667a3f
Cristian Sagula <cristian@Cristians-MacBook-Pro.local>
2025-03-17 22:02:08 -0300

    scrollbar priority

--- components/ScrollBanner/ScrollBanner.tsx
+++ components/ScrollBanner/ScrollBanner.tsx
@@ -6,7 +6,7 @@ const ScrollBanner = ({ lang }: { lang: LangType }) => {
   return (
     <div className='h-screen border-r border-white/5 p-9 flex-col justify-between max-w-[7.5rem] hidden lg:flex'>
       <Link href={`/${lang}`} className='relative'>
-        <Image src='/images/logo.svg' alt='Estudio/nk' width={52} height={24} />
+        <Image src='/images/logo.svg' alt='Estudio/nk' width={52} height={24} priority loading='eager' quality={75} />
       </Link>
       <div className="-rotate-90 mb-10 relative before:content-[''] before:absolute before:size-[0.5625rem] before:bg-primary-green before:rounded-full before:-left-6 before:top-[calc(-50%+1.60rem)] before:-translate-y-1/2">
         <span className='uppercase text-white/50 text-[0.625rem] inline-block w-[7.5rem]'>scroll to discover</span>
